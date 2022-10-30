1189f86c8ead3c8ef24dcb86a2a028b5a799a311
Savana Rohit <92589029+savanarohit@users.noreply.github.com>
2022-10-30 21:20:04 +0530

    Update tools.sh

--- tools.sh
+++ tools.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-sudo amazon-linux-extras install epel && yum -y install net-tools links gcc nmap wget telnet epel-release p7zip rkhunter glances 
+sudo amazon-linux-extras install epel && yum install net-tools links gcc nmap wget telnet epel-release p7zip rkhunter glances -y 
