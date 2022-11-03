96527bfa872508e0aa691331f62a0677ea6c5400
Savana Rohit <rohitsavana18@gmail.com>
2022-11-03 11:30:39 +0530

    Create ubuntu dir with tools.sh script

--- ubuntu/tools.sh
+++ ubuntu/tools.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 sudo apt-get update -y
 sudo apt-get upgrade -y
+sudo apt-get install build-essential python3 python3-dev
 sudo apt-get install iotop powertop apachetop nmap munin iptraf -y
 sudo snap install links glances 
