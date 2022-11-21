8be246c5fea6a872161004c041c4f895163d0647
Savana Rohit <92589029+savanarohit@users.noreply.github.com>
2022-11-21 09:24:01 +0530

    Update tools.sh

--- ubuntu/tools.sh
+++ ubuntu/tools.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 sudo apt-get update -y
 sudo apt-get upgrade -y
-sudo apt-get install build-essential python3 python3-dev
+sudo apt-get install build-essential python python3-dev python3-venv python3-pip
 sudo apt-get install iotop powertop apachetop nmap munin iptraf -y
-sudo snap install links glances 
+sudo snap install links glances
