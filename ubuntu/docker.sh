3c763aa6de4fb278ce5f0666053c0d2b6a280112
Savana Rohit <92589029+savanarohit@users.noreply.github.com>
2022-12-24 20:30:44 +0530

    Update  docker.sh

--- ubuntu/docker.sh
+++ ubuntu/docker.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
-sudo apt update && sudo apt install apt-transport-https ca-certificates curl software-properties-common -y
+sudo apt install apt-transport-https ca-certificates curl software-properties-common
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu focal stable"
-apt-cache policy docker-ce -y
+apt-cache policy docker-ce -y 
+sudo apt update
 sudo apt install docker-ce -y && sudo apt install docker-compose -y
 sudo usermod -aG docker $USER && newgrp docker
